7fa1c61dd16d5c79dadf4c612c755e1481b88af4
rikakohira <rikako.takase88@gmail.com>
2023-02-05 07:29:00 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+  //コメント
+
+  /* コメント
+  */
+
+  // echo(123);
+  // echo('<br>');
+  // echo('hello');
+  // echo('<br>');  
+  // echo("hello");
+  // echo('<br>');
+  // echo('hell""o');
+  // echo('<br>');
+  // echo('test');
+  // echo('<br>');
+
+  // 変数
+  // $test = 123;
+  // $test = 'テストです';
+
+
+  // var_dump($test);
+  // echo $test;
+
+  // $test_1 = 123;
+	// $test_2 = 456;
+	
+	// $test_3 = $test_1.$test_2;
+	// var_dump($test_3);
+
+  // const MAX = 10;
+  // echo MAX;
+
+  $array = [1, 2, 3];
+  echo $array[1];
+?>
