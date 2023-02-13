92a3cab80b0955978c761f5907b3b92415ab6c61
rikakohira <rikako.takase88@gmail.com>
2023-02-14 06:52:00 +0900

    33. 処理を止める exit

--- index.php
+++ index.php
@@ -306,8 +306,15 @@
 // echo __FILE__;
 
 // $test = 123;
-echo $test;
-phpinfo();
+// echo $test;
+// phpinfo();
 
+$test = 123;
+$test_2 = 456;
+
+echo $test;
+var_dump($test);
+exit;
+echo $test_2;
 
 ?>
