d9a9408707743ae4e9ad94b83ebf9bdab29255ad
rikakohira <rikako.takase88@gmail.com>
2023-03-22 09:11:23 +0900

    43. Bootstrap4 その1

--- form/input.php
+++ form/input.php
@@ -25,9 +25,18 @@ if(!empty($_POST['btn_submit'])){
 }
 ?>
 
-<!DOCTYPE html>
-<meta charset="utf-8">
-<head></head>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
 <body>
 
 <!-- 確認画面 -->
@@ -108,11 +117,17 @@ $token = $_SESSION['csrfToken'];
 <?php echo '</ul>' ; ?>
 
 <?php endif ;?>
-
-<form method="POST" action="input.php">
-氏名
-<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']) ;} ?>">
-<br>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+    <form method="POST" action="input.php">
+    <div class="form-group">
+      <label for="your_name">氏名</label>
+      <input type="text" class="form-control" id="your_name" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']) ;} ?>" required>
+    </div>
+    </div>
+  </div>
+</div>
 メールアドレス
 <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']) ;} ?>">
 <br>
@@ -151,6 +166,14 @@ $token = $_SESSION['csrfToken'];
 </form>
 <?php endif; ?>
 
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+-->
 </body>
 </html>
 
